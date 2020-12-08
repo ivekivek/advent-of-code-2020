fe97b89b25f165a470351bb9c791eada9c2a210c
Ivan <46201857+ivekivek@users.noreply.github.com>
2020-12-08 14:08:39 +0100

    Add files via upload

--- day02/day02.ts
+++ day02/day02.ts
@@ -1,5 +1,23 @@
 import * as fs from 'fs'
 
+function solve(input) {
+    let counter=0
+    for (let i=0; i<input.length; i++) {
+        let policy = input[i].match(/\b([0-9]|[1-9][0-9]|100)\b-\b([0-9]|[1-9][0-9]|100)\b [a-z]: [a-z]\w+/g)[0].split(':')
+        let passport = input[i].match(/\b([0-9]|[1-9][0-9]|100)\b-\b([0-9]|[1-9][0-9]|100)\b [a-z]: [a-z]\w+/g)[0].split(':')
+        let tmp = policy[0].split(' ')
+    
+        console.log(tmp[1], passport[1][parseInt(tmp[0].split('-')[0])-1], passport[1][parseInt(tmp[0].split('-')[1])-1])
+        
+        if (tmp[1] === passport[1][parseInt(tmp[0].split('-')[0])-1] && tmp[1] !== passport[parseInt(tmp[0].split('-')[1])-1]) {
+            counter+=1
+        } else if (tmp[1] === passport[1][parseInt(tmp[0].split('-')[0])-1] && tmp[1] !== passport[parseInt(tmp[0].split('-')[1])-1]) {
+            counter+=1
+        }
+    }
+    return counter
+}
+
 let result = 0
 let arr = []
 let file = 'input.txt'
@@ -9,8 +27,10 @@ input.forEach(i => {
     arr.push(i)
 })
 
-arr.forEach(i => {
-    i = i.split(':')
+let array = []
+
+arr.forEach(a => {
+    let i = a.split(':')
     let policy = i[0]
     let password = i[1].replace(' ', '').replace('\n', '')
     policy = policy.split(' ')
@@ -27,8 +47,8 @@ arr.forEach(i => {
     let num2 = parseInt(nums[1])
 
     if (counter >= num1 && counter <= num2) {
-        result+=1
+        array.push(a)
     }
 })
 
-console.log(result)
+console.log(solve(array))
